a42911a06fd78e1401a0d5cc17c3ecbc9d004802
Maria Puchkina <mariapuchkina@Marias-iMac.local>
2022-02-21 22:23:48 +0000

    “week03”

--- secondstring.py
+++ secondstring.py
@@ -1,2 +1,9 @@
 # my solution to weekly task 03
 # author: Maria Puchkina
+# the program asks a use to input a sentence and output every second letter in reverse
+
+# input the sentence
+string = (input("Please enter sentence: "))
+
+# reversing the string with taking every 2d letter and printing the result
+print(string[::-2])
