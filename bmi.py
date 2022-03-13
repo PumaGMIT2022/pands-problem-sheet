df3a551e7adb9f689915f137d69ad50a12f82f57
Maria Puchkina <mariapuchkina@Marias-iMac.local>
2022-03-13 22:25:22 +0000

    “weeklytask02”

--- bmi.py
+++ bmi.py
@@ -2,6 +2,6 @@
 # author: Maria Puchkina
 # program calculates somebody's Body Mass Index (BMI)
 # BMI is the body mass divided by the square of the body height
-height = float(input("Input your height in Meters: "))
+height = float(input("Input your height in Centimeters: "))
 weight = float(input("Input your weight in Kilogram: "))
-print("Your BMI is: ", round(weight / (height * height), 2))
+print("Your BMI is: ", round(weight * 10000 / (height * height), 2))
